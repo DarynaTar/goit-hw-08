d9fd7b26d1823052b3d3cd9ae46a0f06535c1ef6
DarynaTar <119743833+DarynaTar@users.noreply.github.com>
2023-04-30 21:22:24 +0300

    create gallery commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log('Hello world');
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const listGallery = document.querySelector('.gallery');
+
+const itemGallery = galleryItems
+  .map(({preview, original, description}) => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </li>
+  `)
+  .join('');
+
+listGallery.insertAdjacentHTML('beforeend', itemGallery);
+
+const lightboxOptions = {
+  captions: true,
+  captionDelay: 250,
+  captionPosition: 'bottom',
+  captionsData: 'alt',
+};
+
+const lightbox = new SimpleLightbox('.gallery a', lightboxOptions);
+
