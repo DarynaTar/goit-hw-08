c9cd1e994b7ff1dee469e7c98b7f731d03085944
DarynaTar <119743833+DarynaTar@users.noreply.github.com>
2023-05-01 08:21:52 +0300

    update time video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from "@vimeo/player";
 
+
+const iframe = document.querySelector('iframe')
+const player = new Player(iframe)
+
+const onTimeUpdate = function(data){
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data.seconds))
+}
+
+player.on('onTimeUpdate', onTimeUpdate)
+
+const savedTime = JSON.parse(localStorage.getItem('videoplayer-current-time'))
+    if(savedTime) {
+        player.setCurrentTime(savedTime)
+    }
