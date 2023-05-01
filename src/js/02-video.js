8423c9285e6c138d7f10a0f37ce5fa412965ba3c
DarynaTar <119743833+DarynaTar@users.noreply.github.com>
2023-05-01 12:48:04 +0300

    add throttle commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,12 @@
 import Player from "@vimeo/player";
-
+import throttle from "lodash.throttle";
 
 const iframe = document.querySelector('iframe')
 const player = new Player(iframe)
 
-const onTimeUpdate = function(data){
+const onTimeUpdate = throttle(function(data){
     localStorage.setItem('videoplayer-current-time', JSON.stringify(data.seconds))
-}
+}, 1000)
 
 player.on('onTimeUpdate', onTimeUpdate)
 
