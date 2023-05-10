e98203be33b239a7988f7412abd5c6876265bfce
DarynaTar <119743833+DarynaTar@users.noreply.github.com>
2023-05-10 17:25:01 +0300

    update saved data-form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,43 +1,41 @@
 import throttle from "lodash.throttle"
+
 const form = document.querySelector('.feedback-form')
 
 form.addEventListener('input', throttle(handleInput, 500))
 
 function handleInput(event){
-
     const feedbackForm = {
         email: form.elements.email.value,
         message: form.elements.message.value
     }
 
-localStorage.setItem('feedback-form-state', JSON.stringify(feedbackForm))
+    localStorage.setItem('feedback-form-state', JSON.stringify(feedbackForm))
 
-const savedFeedback = JSON.parse(localStorage.getItem('feedbackForm'))
-if(savedFeedback) {
-email.value = savedFeedback.email
-message.value = savedFeedback.message
-}
-}
+    const savedFeedback = JSON.parse(localStorage.getItem('feedback-form-state'))
 
+    if(savedFeedback) {
+        form.elements.email.value = savedFeedback.email
+        form.elements.message.value = savedFeedback.message
+    } else {
+        form.elements.email.value = ''
+        form.elements.message.value = ''
+    }
+}
 
 form.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event){
     event.preventDefault();
     const {elements: {email, message}} = event.currentTarget
+
     if(email.value && message.value) {
-        console.log(`Email: ${email.value}, message: ${message.value}`)  
+        console.log(`Email: ${email.value}, message: ${message.value}`)
+        localStorage.removeItem('feedback-form-state')
+        email.value = '';
+        message.value = '';
     } else {
+        handleInput()
         alert("Fill in the fields")
     }
-
-    localStorage.removeItem('feedback-form-state')
-    // event.currentTarget.reset()
-    email.value = '';
-    message.value = '';
 }
-
-
-
-
-
